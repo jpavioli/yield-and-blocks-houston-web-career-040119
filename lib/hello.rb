1052b276140112a9cf109659c0b9960b1b1535bf
Jusitn Avioli <justin.avioli@gmail.com>
2019-03-23 21:46:59 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,11 +1,3 @@
-
-
-hello_t(["Tim", "Tom", "Jim"]) do |name|
-  if name.start_with?("T")
-    puts "Hi, #{name}"
-  end
-end
-
 def hello_t(array)
   i = 0
   while i < array.length
@@ -13,4 +5,10 @@ def hello_t(array)
     i = i + 1
   end
   array
+end
+
+hello_t(["Tim", "Tom", "Jim"]) do |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  end
 end
