3d076e6a95b651e282ea5058c1d8f8011421f13d
Jusitn Avioli <justin.avioli@gmail.com>
2019-03-23 22:35:22 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,7 +1,7 @@
 def hello_t(array)
   i = 0
   while i < array.length
-    x = yield array[i]
+    yield array[i]
     i = i + 1
   end
   array
@@ -10,9 +10,5 @@ end
 hello_t(["Tim", "Tom", "Jim"]) do |name| 
   if name.start_with?("T")
     puts "Hi, #{name}"
-  elsif name.empty? 
-    puts "Hey! No block was given!"
-  else
-    puts name.upcase
   end
 end
